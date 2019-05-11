9eb47f791a0b1c7f7d2a671e67833b93c0687e8d
czmx <czmx.github.io>
2019-05-11 23:43:14 +0800

    xuexi

--- src/Type/Definition/ScalarType.php
+++ src/Type/Definition/ScalarType.php
@@ -10,11 +10,9 @@ use GraphQL\Utils\Utils;
 use function is_string;
 
 /**
- * Scalar Type Definition
+ * 标量类型定义
  *
- * The leaf values of any request and input values to arguments are
- * Scalars (or Enums) and are defined with a name and a series of coercion
- * functions used to ensure validity.
+ * 参数的任何请求和输入值的叶值都是 Scalar 或 Enums 类型，并使用名称和一系列用于确保有效性的强制函数进行定义
  *
  * Example:
  *
@@ -29,10 +27,18 @@ use function is_string;
  */
 abstract class ScalarType extends Type implements OutputType, InputType, LeafType, NullableType, NamedType
 {
-    /** @var ScalarTypeDefinitionNode|null */
+    /**
+     * AST 节点对象
+     *
+     * @var ScalarTypeDefinitionNode|null
+     */
     public $astNode;
 
-    /** @var ScalarTypeExtensionNode[] */
+    /**
+     * AST 扩展节点对象
+     *
+     * @var ScalarTypeExtensionNode[]
+     */
     public $extensionASTNodes;
 
     /**
@@ -40,11 +46,11 @@ abstract class ScalarType extends Type implements OutputType, InputType, LeafTyp
      */
     public function __construct(array $config = [])
     {
-        $this->name              = $config['name'] ?? $this->tryInferName();
-        $this->description       = $config['description'] ?? $this->description;
-        $this->astNode           = $config['astNode'] ?? null;
+        $this->name = $config['name'] ?? $this->tryInferName();
+        $this->description = $config['description'] ?? $this->description;
+        $this->astNode = $config['astNode'] ?? null;
         $this->extensionASTNodes = $config['extensionASTNodes'] ?? null;
-        $this->config            = $config;
+        $this->config = $config;
 
         Utils::invariant(is_string($this->name), 'Must provide name.');
     }
